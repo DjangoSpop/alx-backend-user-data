0cea24ef1f1c663b527adb80018ad0de9ddfcef2
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-18 14:45:50 +0300

    we can

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """create module filtered_logger"""
 import re
 from typing import List
+import PII
 import logging
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
@@ -10,16 +11,31 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     return message
 
 class RedactingFormatter(logging.Formatter):
+    
     """ Redacting Formatter class
         """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-
+        self.fields = fields    
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-    
+        message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+ 
+    def get_logger() -> logging.Logger:
+        """returns a logger object"""
+        logger = logging.getLogger("user_data")
+        logger.setLevel(logging.INFO)
+        logger.propagate = False
+
+        formatter = RedactingFormatter(fields=PII_FIELDS)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+
+        return logger
+       
