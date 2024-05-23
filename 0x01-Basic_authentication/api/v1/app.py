22aec5c88f8f2e3da6323c2da9a93e509989a8a4
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-24 02:05:09 +0300

    Refactor error handling for unauthorized requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,11 @@ elif os.getenv("AUTH_TYPE") == "basic_auth":
 
     auth = BasicAuth()
 
+@app.errorhandler(401)
+def unauthorized() -> str:
+    """Unauthorized handler."""
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(e):
@@ -30,12 +35,6 @@ def forbidden(e):
 def not_found(error) -> str:
     """Not found handler."""
     return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler."""
-    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(401)
