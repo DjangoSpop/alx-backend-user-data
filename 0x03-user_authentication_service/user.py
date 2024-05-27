dc1fb4e40ce2502d1db1ad83c1f6004115921c86
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-27 18:34:32 +0300

    we cccc

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(id='%s', email='%s', hashed_password='%s')>" % (
+                                self.id, self.email, self.hashed_password)
