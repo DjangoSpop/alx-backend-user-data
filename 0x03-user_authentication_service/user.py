f2e10d36712cfc68bc7652456c78e33b3f977374
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2024-05-29 14:38:34 +0300

    chore: Refactor User class __repr__ method in user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+# Module creation of the user table database
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-"""_summary_Returns:_type_: _description_"""
+
 Base = declarative_base()
 
 
 class User(Base):
+    """_summary_
+    Args:
+        Base (_type_): _description_
+    Returns:
+        _type_: _description_
+    """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
@@ -16,6 +22,10 @@ class User(Base):
     reset_token = Column(String(250), nullable=True)
 
     def __repr__(self):
-        return f"<User(id={self.id}), email='{self.email}'"
+        """_summary_
+        Returns:
+            _type_: _description_
+        """
+        return f"<User(id={self.id}), email='{self.email}'>"
         # return "<User(id='%s', email='%s', hashed_password='%s')>" % (
-        #                         self.id, self.email, self.hashed_password)
+        #     self.id, self.email, self.hashed_password)
